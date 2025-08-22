12c90bfd00dbd231198ed3118d8bfecacac4dc38
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-08-22 15:56:27 +0530

    Add .gitignore and implement chat functionality in server

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,60 @@
-import 'dotenv/config'
-import express, {Express, Request, Response} from "express"
-import {MongoClient} from "mongodb"
+import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+import { MongoClient } from "mongodb";
+import { callAgent } from "./agent";
 
-const app: Express = express()
+const app: Express = express();
 
-import cors from 'cors'
-app.use(cors())
-app.use(express.json())
+import cors from "cors";
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGODB_URL as string);
+
+export async function startServer() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("MongoDB connected");
+
+    app.get("/", (req: Request, res: Response) => {
+      res.send("LangGraph Agent Server");
+    });
+
+    app.post("/chat", async (req: Request, res: Response) => {
+      const initialMessage = req.body.message;
+      const threadId = Date.now().toString();
+      console.log(initialMessage);
+      try {
+        const response = await callAgent(client, initialMessage, threadId);
+        res.json({ threadId, response });
+      } catch (error) {
+        console.error("Error starting conversation", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    app.post("chat/:threadId", async (req: Request, res: Response) => {
+      const { threadId } = req.params;
+      const { message } = req.body;
+      try {
+        const response = await callAgent(client, message, threadId);
+        res.json({ response });
+      } catch (error) {
+        console.error("Error in chat: ", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on: ${PORT}`);
+        
+    })
+  } catch (error) {
+    console.error('Error connecting to MongoDB: ', error);
+    process.exit(1);
+  }
+}
+
+// startServer()
