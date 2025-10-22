a2de228ba087f5c177458ae2a0b671d5676c8ef0
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-10-22 23:50:11 +0530

    AI model connected and tested

--- app/playground/[projectId]/page.tsx
+++ app/playground/[projectId]/page.tsx
@@ -24,7 +24,9 @@ const PlayGround = () => {
   const params = useSearchParams();
   const frameId = params.get("frameId");
   const [frameDetails, setFrameDetails] = useState<Frame>();
-  //   console.log(frameId);
+  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState<Messages[]>([]);
+  const [generatedCode, setGeneratedCode] = useState<string>();
 
   useEffect(() => {
     frameId && GetFrameDetails();
@@ -38,13 +40,68 @@ const PlayGround = () => {
     setFrameDetails(result.data);
   };
 
+  const SendMessage = async (userInput: string) => {
+    setLoading(true);
+
+    // Add user message to chart
+    setMessages((prev: any) => [...prev, { role: "user", content: userInput }]);
+
+    const result = await fetch("/api/ai-model", {
+      method: "POST",
+      body: JSON.stringify({
+        messages: [{ role: "user", content: userInput }],
+      }),
+    });
+
+    let aiResponse = "";
+    let isCode = false;
+    const reader = result.body?.getReader();
+    const decoder = new TextDecoder();
+
+    while (true) {
+
+      //@ts-ignore
+      const { done, value } = await reader?.read();
+      if (done) break;
+
+      const chunk = decoder.decode(value, { stream: true });
+      aiResponse += chunk;
+
+      if (!isCode && aiResponse.includes("```html")) {
+        isCode = true;
+        const index = aiResponse.indexOf("```html") + 7;
+        const initialCodeChunk = aiResponse.slice(index);
+        setGeneratedCode((prev: any) => prev + initialCodeChunk);
+      } else if (isCode) {
+        setGeneratedCode((prev: any) => prev + chunk);
+      }
+    }
+
+    // After streaming end
+    if (!isCode) {
+      setMessages((prev: any) => [
+        ...prev,
+        { role: "assistant", content: aiResponse },
+      ]);
+    } else {
+      setMessages((prev: any) => [
+        ...prev,
+        { role: "assistant", content: "Your code is ready!" },
+      ]);
+    }
+    setLoading(false);
+  };
+
   return (
     <div>
       <PlayGroundHeader />
 
       <div className="flex">
         {/* Chat section */}
-        <ChatSection messages={frameDetails?.chatMessages ?? []} />
+        <ChatSection
+          messages={messages ?? []}
+          onSend={(input: string) => SendMessage(input)}
+        />
 
         {/* Website design */}
         <WebsiteDesign />
--- app/playground/_components/ChatSection.tsx
+++ app/playground/_components/ChatSection.tsx
@@ -1,12 +1,25 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { Messages } from "../[projectId]/page";
+import { ArrowUp } from "lucide-react";
+import { useState } from "react";
 
 type Props = {
   messages: Messages[];
+  onSend: any;
 };
 
-const ChatSection = ({ messages }: Props) => {
+const ChatSection = ({ messages, onSend }: Props) => {
+  const [input, setInput] = useState<string>();
+
+  const handleSend = () => {
+    if (!input?.trim()) return;
+    onSend(input);
+    setInput("");
+  };
+
   return (
-    <div className="w-96 h-[90vh] shadow p-4">
+    <div className="w-96 h-[90vh] shadow p-4 flex flex-col">
       {/* Message */}
       <div className="flex-1 overflow-y-auto p-4 space-y-3 flex flex-col">
         {messages?.length === 0 ? (
@@ -34,6 +47,17 @@ const ChatSection = ({ messages }: Props) => {
       </div>
 
       {/* Footer */}
+      <div className="p-3 border-t flex items-center gap-2">
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Describe your website design idea..."
+          className="flex-1 resize-none border rounded-lg px-3 py-2 focus:outline-none focus:ring-2"
+        />
+        <Button size={"icon-lg"} className="rounded-full" onClick={handleSend}>
+          <ArrowUp />
+        </Button>
+      </div>
     </div>
   );
 };
