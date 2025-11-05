230b2645f06c942a886937db61047c106bcb251d
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-11-05 16:26:40 +0530

    PlayGround Header Icon made redirect to worspace

--- app/playground/_components/PlayGroundHeader.tsx
+++ app/playground/_components/PlayGroundHeader.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { OnSaveContext } from "@/context/OnSaveContext";
 import Image from "next/image";
+import Link from "next/link";
 import { useContext } from "react";
 
 const PlayGroundHeader = () => {
-  const { onSaveDate, setOnSaveDate } = useContext(OnSaveContext);
+  const { setOnSaveDate } = useContext(OnSaveContext);
   return (
-    <div className="flex justify-between items-center p-4 shadow">
-      <Image src={"/logo.svg"} alt="logo" width={40} height={40} />
+    <div className="flex justify-between items-center p-5 shadow">
+      <Link href={"/workspace"} className="flex items-center gap-2">
+        <Image src={"/logo.svg"} alt="logo" width={35} height={35} />
+        <h2 className="font-bold text-xl">AI Web Creator</h2>
+      </Link>
       <Button onClick={() => setOnSaveDate(Date.now())}>Save</Button>
     </div>
   );
